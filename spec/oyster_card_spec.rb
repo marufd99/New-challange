ffbe104067392de44381e053754f6f6a3701e185
marufd99 <maruf.d99@gmail.com>
2019-11-07 15:45:52 +0000

    Lets commit

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -15,13 +15,5 @@ describe OysterCard do
   it 'expect to raise an error if over 90' do
   expect {subject.top_up(95).to raise_error ("Maximum limit reached")}
   end
-#it 'expect to have an limit of 90' do
-#  expected {}
-end
 
-#describe OysterCard do
-#  oyster_card = OysterCard.new
-#it {expect(oyster_card.top_up)}.to eq(90)}
-#end
-#  it {expect(oyster_card.money).to respond_to(:top_up).with(1).argument}
-#end
+end
