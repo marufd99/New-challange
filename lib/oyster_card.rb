b2c43fb3cf57f767783a09c757bcd1efa9032260
marufd99 <maruf.d99@gmail.com>
2019-11-12 08:15:47 +0000

    Added staion and touch function.

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,23 +1,41 @@
 class OysterCard
+  attr_reader :balance, :entry_station
+  attr_accessor :in_journey
+
+  CHARGE = 1.20
   MIN_BALANCE = 1
   MAXIMUM_BALANCE = 90
-attr_reader :balance
-#attr_accessor :in_journey?
+
 def initialize
    @balance = 0
    @in_journey = false
+   @entry_station != nil
  end
+
  def top_up(amount)
-   @balance += amount
-   fail "Maximum balance of #{MAXIMUM_BALANCE} reached" if (@balance += amount > 90)
+   if (balance + amount > MAXIMUM_BALANCE)
+   fail "Maximum balance of #{MAXIMUM_BALANCE} reached"
  end
- def deduct(amount)
    @balance += amount
  end
- 
- #def touch_in
-  # fail "Less the £1" if @balance < MIN_BALANCE
-   #@in_journey = true
- #end
+
+
+ def touch_in(station)
+   fail "Less than £1" if @balance < MIN_BALANCE
+   @in_journey = true
+   @entry_station = station
+ end
+ def touch_out
+   deduct(CHARGE)
+   @in_journey = false
+   @entry_station = nil
+ end
+
+
+ private
+
+
+def deduct(amount)
+  @balance -= amount
 end
-#end
+end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,6 +4,10 @@ require 'oyster_card'
 describe OysterCard do
   it {expect(subject).to be_instance_of(OysterCard)}
 
+  describe '#balance' do
+    it{is_expected.to respond_to(:balance)}
+#  end
+
 # oyster_card = OysterCard.new
   it {expect(subject).to respond_to(:balance)}
 # oyster_card = OysterCard.new
@@ -15,26 +19,42 @@ describe OysterCard do
   it 'expect to raise an error if over 90' do
   expect {subject.top_up(95).to raise_error ("Maximum limit reached")}
   end
-it 'expect to deduct money from oystercard' do
-  expect {(subject).to respond_to(:deduct).with(1).argument}
-end
+#it 'expect to deduct money from oystercard' do
+#  expect {(subject).to respond_to(:deduct).with(1).argument}
+#end
 it 'is initially not in a journey' do
   expect{(subject).not_to be(:in_journey?)}
 end
- it "can touch in" do
-  subject.touch_in
-  expect{(subject.touch_in).to be(:in_journey?)}
-end
-it "can touch out" do
-  subject.touch_in
-  subject.touch_out
-  expect(subject).not_to be(:in_journey?)
-end
-context 'when the minimum balance limit has been reached'
-min_balance = OysterCard::MIN_BALANCE
-it "raise an error, limit reached " do
-  subject.balance < min_balance
-  expect {subject.touch_in }.to raise_error("Less then 1")
+ #it "can touch in" do
+  #subject.touch_in
+#  expect{(subject.touch_in).to be(:in_journey?)}
+#end
+#it "can touch out" do
+#  subject.touch_in
+#  subject.touch_out
+#  expect(subject).not_to be(:in_journey?)
+#end
+it {is_expected.to respond_to(:touch_in) }
+it {is_expected.to respond_to(:touch_out) }
 
+context 'when the minimum balance limit has been reached' do
+min_balance = OysterCard::MIN_BALANCE
+it "raise an error limit reached " do
+  subject.balance < min_balance
+expect{subject.touch_in(@station)}.to raise_error("Less than £1")
+end
+end
+
+it 'should update the balance with journey ' do
+  expect{subject.touch_out}.to change{subject.balance}.by -1.20
+end
+it "remember entry station on touch in" do
+      subject.top_up(30)
+      subject.touch_in("Barbican")
+      expect(subject.entry_station).to eq("Barbican")
+end
+it "clears entry station on touch out" do
+    expect(subject.entry_station).to eq(nil)
+end
 end
 end
